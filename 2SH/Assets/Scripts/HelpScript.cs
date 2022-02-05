9f7ac2bf2f4e1e8622c54df16b90ce50d72202db
Haaris Iqbal <haaris.n.iqbal@gmail.com>
2022-02-05 14:48:49 +0000

    Help Script
    Adding a help script to return the name and descriptions from a List.

---
+++ 2SH/Assets/Scripts/HelpScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpScript : Command
+{
+    public HelpScript(string name)
+    {
+        this.name = name;
+        this.desctiption = "List all possible commands.";
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
