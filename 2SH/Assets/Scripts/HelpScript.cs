79b8ba1261255f31ee528133779cab90a5583df3
Haaris Iqbal <haaris.n.iqbal@gmail.com>
2022-02-05 14:50:14 +0000

    Dead Code
    Removed unused functions.

--- 2SH/Assets/Scripts/HelpScript.cs
+++ 2SH/Assets/Scripts/HelpScript.cs
@@ -9,16 +9,4 @@ public class HelpScript : Command
         this.name = name;
         this.desctiption = "List all possible commands.";
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
