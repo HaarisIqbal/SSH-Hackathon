36242563540890a004caf50a3797451da17a9368
Haaris Iqbal <haaris.n.iqbal@gmail.com>
2022-02-05 12:18:11 +0000

    Log Script
    Created new folder for Scripts and added new Script for Logging messages in console.

---
+++ 2SH/Assets/Scripts/LogScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogScript : MonoBehaviour
+{   
+    // Log will accept input and return a String
+    string log() 
+    {
+        string output; // Output of Log.
+
+        // Output to be computed here.
+
+        return output;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
