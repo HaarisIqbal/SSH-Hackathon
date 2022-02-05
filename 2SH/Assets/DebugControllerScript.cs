67096867a102cd964d310d873e4f1ac1ffde5337
Stefan <zhac332@live.rhul.ac.uk>
2022-02-05 16:17:17 +0000

    Started a method that returns the result of a given command which is valid. Removed 'focus' and 'list' as they are complicating the game too much.

--- 2SH/Assets/DebugControllerScript.cs
+++ 2SH/Assets/DebugControllerScript.cs
@@ -26,13 +26,32 @@ public class DebugControllerScript : MonoBehaviour
         {
             if (CheckCommand(InputF.text))
             {
-                Con.Log("\n   > " + InputF.text);
+                Con.Log("\n   > " + Result(InputF.text));
+                InputF.text = "";
+                Scroll.verticalNormalizedPosition = 0f;
+            }
+            else
+            {
+                Con.Log("\n   > invalid command");
                 InputF.text = "";
                 Scroll.verticalNormalizedPosition = 0f;
             }
         }
     }
 
+
+    /// <summary>
+    /// Returns the content and the results of the command that the method will detect.
+    /// Based on the commands that we implemented, we will give certain results.
+    /// </summary>
+    private string Result(string command)
+    {
+        return null;
+    }
+
+    /// <summary>
+    /// Just checks if the said command is valid or not.
+    /// </summary>
     private bool CheckCommand(string text)
     {
         string keyword = text.Split(' ')[0];
@@ -45,9 +64,7 @@ public class DebugControllerScript : MonoBehaviour
     private void PopulateList()
     {
         AcceptedCommands.Add("help");
-        AcceptedCommands.Add("list");
         AcceptedCommands.Add("watk");
         AcceptedCommands.Add("dehash");
-        AcceptedCommands.Add("focus");
     }
 }
