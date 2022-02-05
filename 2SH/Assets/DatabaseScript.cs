b99b85f075a1e76b3310e7cb8b430a80898071cb
Stefan <zhac332@live.rhul.ac.uk>
2022-02-05 18:12:31 +0000

    Added a method that generates the data of the database.

--- 2SH/Assets/DatabaseScript.cs
+++ 2SH/Assets/DatabaseScript.cs
@@ -4,15 +4,72 @@ using UnityEngine;
 
 public class DatabaseScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Is the database protected?")]
+    [SerializeField] private bool IsProtected;
+
+    private List<string> usernames;
+    private List<string> passwords;
+    private int numberEntries;
+
+    private void Start()
     {
-        
+        GenerateData();
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Generates random usernames and random passwords.
+    /// </summary>
+    private void GenerateData()
     {
-        
+        numberEntries = Random.Range(0, 100);
+
+        for (int i = 0; i < numberEntries; i++)
+        {
+            string newUsername = GenerateUsername();
+            string newPassword = GeneratePassword();
+
+            usernames.Add(newUsername);
+            passwords.Add(newPassword);
+        }
+    }
+
+    private string GenerateUsername()
+    {
+        string username = "";
+
+        List<char> letters = new List<char>();
+        List<char> digits = new List<char>();
+        for (char c = '0'; c <= '9'; c++)
+            letters.Add(c);
+        for (char c = 'a'; c <= 'z'; c++)
+            digits.Add(c);
+
+        for (int i = 0; i < 7; i++)
+            username += letters[Random.Range(0, letters.Count + 1)];
+        for (int i = 0; i < 3; i++)
+            username += digits[Random.Range(0, digits.Count + 1)];
+
+        return username;
+    }
+
+    private string GeneratePassword()
+    {
+        string password = "";
+
+        List<char> characters = new List<char>();
+        for (char c = '0'; c <= '9'; c++)
+            characters.Add(c);
+        for (char c = 'a'; c <= 'z'; c++)
+            characters.Add(c);
+        characters.Add('!');
+        characters.Add('?');
+        characters.Add('#');
+        characters.Add('@');
+        characters.Add('&');
+
+        for (int i = 0; i < 10; i++)
+            password += characters[Random.Range(0, characters.Count + 1)];
+
+        return password;
     }
 }
