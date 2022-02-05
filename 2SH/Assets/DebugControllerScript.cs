0089bd0c856e5717852e71f4b636fd56d03aee19
Stefan <zhac332@live.rhul.ac.uk>
2022-02-05 15:08:53 +0000

    Refactored the console script so that it handles the texts that are inserted in the console panel and moved some of its original methods to the DebugControllerScript so that it performs checks on certain commands.

--- 2SH/Assets/DebugControllerScript.cs
+++ 2SH/Assets/DebugControllerScript.cs
@@ -8,14 +8,50 @@ public class DebugControllerScript : MonoBehaviour
     [SerializeField] private InputField InputF;
     [SerializeField] private ScrollRect Scroll;
     [SerializeField] private Text ConsoleContent;
+    [SerializeField] private Console Con;
+
+    private List<string> AcceptedCommands;
+
+    private void Start()
+    {
+        AcceptedCommands = new List<string>();
+        PopulateList();
+    }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            ConsoleContent.text += "\n   > " + InputF.text;
-            InputF.text = "";
-            Scroll.verticalNormalizedPosition = 0f;
+            if (CheckCommand(InputF.text))
+            {
+                Con.Log("\n   > " + InputF.text);
+                InputF.text = "";
+                Scroll.verticalNormalizedPosition = 0f;
+            }
         }
+    }
+
+    private bool CheckCommand(string text)
+    {
+        string keyword = text.Split(' ')[0];
+
+        switch(keyword)
+        {
+            case "help":
+                break;
+            case "list":
+                break;
+        }
+
+        return false;
+    }
+
+    private void PopulateList()
+    {
+        AcceptedCommands.Add("help");
+        AcceptedCommands.Add("list");
+        AcceptedCommands.Add("watk");
+        AcceptedCommands.Add("dehash");
+        AcceptedCommands.Add("focus");
     }
 }
