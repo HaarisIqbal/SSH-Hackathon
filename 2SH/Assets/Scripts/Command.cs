87bc88c2e758b7648a24d44fd2c49312a41a6878
Stefan <zhac332@live.rhul.ac.uk>
2022-02-05 16:27:25 +0000

    Added an extra method in the command class that allows all commands to execute and give a certain result.

--- 2SH/Assets/Scripts/Command.cs
+++ 2SH/Assets/Scripts/Command.cs
@@ -8,5 +8,13 @@ public class Command
     protected string Description;
     protected string Syntax;
 
+    /// <summary>
+    /// Method that only checks the syntax of the command.
+    /// </summary>
     public virtual bool CheckSyntax(string command) { return false; }
+
+    /// <summary>
+    /// Method that executes the command (each command has its own execution) and returns the result as a string.
+    /// </summary>
+    public virtual string ExecuteCommand(string command) { return null; }
 }
