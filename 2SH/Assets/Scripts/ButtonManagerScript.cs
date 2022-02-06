6ac6cf35fa477692c655fb33412349b5a7c3dc4f
Stefan <zhac332@live.rhul.ac.uk>
2022-02-06 13:00:50 +0000

    Build the game for windows in a zip folder.

--- 2SH/Assets/Scripts/ButtonManagerScript.cs
+++ 2SH/Assets/Scripts/ButtonManagerScript.cs
@@ -7,33 +7,33 @@ public class ButtonManagerScript : MonoBehaviour
 {
     public void Mission1Button_Clicked()
     {
-        SceneManager.LoadScene("Mission1");
+        SceneManager.LoadScene("Mission 1");
     }
 
     public void Mission2Button_Clicked()
     {
-        SceneManager.LoadScene("Mission2");
+        SceneManager.LoadScene("Mission 2");
     }
 
     public void Mission3Button_Clicked()
     {
-        SceneManager.LoadScene("Mission3");
+        SceneManager.LoadScene("Mission 3");
 
     }
 
     public void Mission4Button_Clicked()
     {
-        SceneManager.LoadScene("Mission4");
+        SceneManager.LoadScene("Mission 4");
     }
 
     public void Mission5Button_Clicked()
     {
-        SceneManager.LoadScene("Mission5");
+        SceneManager.LoadScene("Mission 5");
     }
 
     public void Mission6Button_Clicked()
     {
-        SceneManager.LoadScene("Mission6");
+        SceneManager.LoadScene("Mission 6");
     }
 
 }
