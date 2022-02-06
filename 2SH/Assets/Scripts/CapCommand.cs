c74927700a664cbb17cefb9d71e928759ff72255
sorinsam01 <sorinsam01@outlook.com>
2022-02-06 09:51:10 +0000

    CapCommand object created

---
+++ 2SH/Assets/Scripts/CapCommand.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapCommand : Command
+{
+    public CapCommand()
+    {
+        this.Name = "CAPTURE";
+        this.Syntax = "cap [filename]";
+        this.Description = "Capture a file";
+    }
+
+    public override bool CheckSyntax(string command)
+    {
+        if (command.Contains("cap"))
+        {
+            return true;
+        }
+        
+        return false;
+    }
+
+    public override string ExecuteCommand(string command)
+    {
+        string fileNameToFind = command.Split(' ')[1];
+        string result = "";
+        TextFile file = GameObject.Find("FileSystem").GetComponent<FileSystem>().GetSpecificFile(fileNameToFind);
+
+        if (file.IsNull())
+        {
+            TextFile content = new TextFile("WPAhs", "temp");
+        }
+        else result = "File already exists";
+
+        return result;
+    }
+
+    public string GenerateHashCode()
+    {
+        string numbers = "1,2,3,4,5,6,7,8,9";
+        string alphabet = " A B C D E F G H I J K L M N O P Q R S T U V W X Y Z ";
+        
+
+        string[] nh = numbers.Split(',');
+        string[] ah = alphabet.Split(' ');
+
+        int[] indexes = new int[12];
+        for (int i = 0; i < 12; i++)
+        {
+            indexes[i] = Random.Range(0,nh.Length-1);
+        }
+
+        indexes[5] = Random.Range(0, ah.Length - 1);
+
+        string GenerateHash = "WPA handshake: " + nh[indexes[0]] + nh[indexes[1]] + "-" + nh[indexes[2]] + nh[indexes[3]] +
+            "-" + nh[indexes[4]] + ah[indexes[5]] + "-" + nh[indexes[6]] + nh[indexes[7]] + 
+            "-" + nh[indexes[8]] + nh[indexes[9]] + "-" + nh[indexes[10]] + nh[indexes[11]];
+
+        return GenerateHash;
+    }
+
+
+}
