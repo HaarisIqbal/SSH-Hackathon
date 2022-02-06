8b6a2b1e01c31b80c105f77f42d5765ea2fa85ee
Stefan <zhac332@live.rhul.ac.uk>
2022-02-06 10:33:20 +0000

    Prepared the mission 3 password object with its script.

---
+++ 2SH/Assets/PasswordScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PasswordScript : MonoBehaviour
+{
+    /**
+     * Generate a password that will ONLY BE ACCESSIBLE once the handshake is captured and the decryption is successful.
+     * 
+     * private void GeneratePassword();
+     * public void HashDecrypted(); --> that means I will be able to get the password through the decrypt command (work with only a bool)
+     * 
+     * 
+     * **/
+
+
+    void Start()
+    {
+        
+    }
+}
